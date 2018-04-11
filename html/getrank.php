1bf4b440ad8c15a21048d9f6434a1ff5db5afa23
Xyz <xyz961014@gmail.com>
2018-04-11 23:49:54 +0800

    stat added

--- html/getrank.php
+++ html/getrank.php
@@ -13,7 +13,7 @@ mysqli_query($conn,'set names utf8');
 if ($conn->connect_error) {
     die("Connection failed:".$conn->connect_error);
 }
-$sql = "SELECT *,".$sort." FROM ".$table." WHERE ".$sort." != 0 ORDER BY ".$sort." DESC";
+$sql = "SELECT *,".$sort." FROM ".$table." WHERE ".$sort." != 0 ORDER BY ".$sort.$order;
 if ($asort) {
     $sql = $sql.",".$asort." ASC";
 }
