d1570d3c95c68cab53f2632cf55bfe5d4dda00aa
Xyz <xyz961014@gmail.com>
2018-08-18 01:34:31 +0800

    tufahelper supported del

--- html/tufahelper/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (@$_POST["username"])
-    $username = $_POST["username"];
-if (@$_POST["password"])
-    $password = $_POST["password"];
-echo json_encode(Array($username, $password));
-
-?>
